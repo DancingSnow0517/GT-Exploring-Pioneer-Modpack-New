049c9862376c3ccd7011e92813459f9fabc0b99d
DancingSnow <1121149616@qq.com>
2024-05-22 20:00:32 +0800

    更新GT相关mod，LV任务

--- kubejs/startup_scripts/gtceu/material/materials.js
+++ kubejs/startup_scripts/gtceu/material/materials.js
@@ -4,9 +4,9 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
         .color(0x518539)
         .toolStats(ToolProperty.Builder.of(12, 6, 614400, 3, GTToolType.SOFT_MALLET).build())
 
-    event.create('alumina')
-        .dust().color(0xAAB9B9)
-        .components('2x aluminium', '3x oxygen')
+    // event.create('alumina')
+    //     .dust().color(0xAAB9B9)
+    //     .components('2x aluminium', '3x oxygen')
 
     event.create('vermiculite')
         .ore().dust().color(0xE7E088)
