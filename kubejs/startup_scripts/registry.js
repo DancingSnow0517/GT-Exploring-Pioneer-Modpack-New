577649d0e8b2c30f41be7dddca5e475ea0d8bccf
DancingSnow <1121149616@qq.com>
2024-05-24 14:49:55 +0800

    更新任务书

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -1,3 +1,5 @@
+Platform.mods.kubejs.name = 'GT Exploring Pioneer'
+
 StartupEvents.registry('item', event => {
     event.create('twilight_crystal')
         .tooltip(Text.translatable('tooltip.kubejs.twilight_crystal'))
