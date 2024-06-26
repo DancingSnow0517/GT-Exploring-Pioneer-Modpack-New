65c4f345a47e3f64501330ccbc65f81eb8dd2c21
DancingSnow <1121149616@qq.com>
2024-06-26 15:57:04 +0800

    update

---
+++ kubejs/startup_scripts/gtceu/dimension_marker.js
@@ -0,0 +1,6 @@
+GTCEuStartupEvents.registry('gtceu:dimension_marker', event => {
+    event.create('ad_astra:moon')
+        .tier(1)
+        .icon('minecraft:dirt')
+        .overrideName('Moon')
+})
